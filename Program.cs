1e9190effc6589428e76238e6a8d898a7b0111fb
Tadej Ciglič <tadej.ciglic@gmail.com>
2019-07-24 11:49:23 +0200

    changes to input

--- Program.cs
+++ Program.cs
@@ -7,20 +7,20 @@ namespace SortArrayName
         static void Main(string[] args)
         {
             string[,] data = new string[2, 2];
-
-            for(int name = 0; name < data.GetLength(0); name++)
+            string[] metaData = { "ime", "priimek" };
+            for (int person = 0; person < data.GetLength(0); person++)
             {
-                for (int lastName = 0; lastName < data.GetLength(1); lastName++)
+                for (int attribute = 0; attribute < data.GetLength(1); attribute++)
                 {
-                    Console.WriteLine($"Ime in priimek za {name + 1}. :");
-                    data[name , lastName] = Console.ReadLine();
+                    Console.WriteLine($"Vpiši {metaData[attribute]} za osebo {person + 1}. :");
+                    data[person, attribute] = Console.ReadLine();
                 }
             }
 
             for (int i = 0; i < data.GetLength(0); i++)
             {
 
-                Console.Write(i+1 + ". ");
+                Console.Write(i + 1 + ". ");
                 for (int j = 0; j < data.GetLength(0); j++)
                 {
                     Console.Write($"{data[i, j]}" + " ");
